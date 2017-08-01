94a71162e8ca8db3cdc6736a93349e6829c4d2e9
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2017-08-01 17:44:28 -0600

    add code for test

---
+++ R/main.R
@@ -0,0 +1,44 @@
+#' Test for equality of covariance matrices
+#'
+#' Uses Roy's union-intersection principle for testing for equality of covariance matrices between
+#' two samples. Also provides p-values.
+#'
+#' @param X matrix of size n1 x p
+#' @param Y matrix of size n2 x p
+#' @param inference Method for computing p-value.
+#' @param nperm Number of permutations. See details.
+#' @return A list containing the test statistic and the p-value.
+#' @export
+#' @importFrom corpcor fast.svd
+#' @importFrom RMTstat ptw
+test_covequal <- function(X, Y, inference = c("TW", "permutation"), nperm) {
+    stopifnot(is.matrix(X), is.matrix(Y),
+              ncol(X) == ncol(Y))
+
+    inference <- match.arg(inference, c("TW", "permutation"))
+    if (missing(nperm)) {
+        nperm <- switch(inference,
+                        "TW" = 25,
+                        "permutation" = 500)
+    }
+    stopifnot(is.integer(nperm))
+
+    p <- ncol(X)
+    n1 <- nrow(X)
+    n2 <- nrow(Y)
+
+    # Compute test statistic
+    test_stat <- if (n1 < n2) compute_largest_root(X, Y) else compute_largest_root(Y, X)
+
+    # Perform permutations
+    null_dist <- permutation(X, Y, nperm)
+
+    # Compute p-value
+    p_value <- switch(inference,
+                      "TW" = fit_locationScale(test_stat, null_dist),
+                      "permutation" = mean(null_dist < test_stat))
+
+    return(list("test_statistic" = test_stat,
+                "pvalue" = p_value))
+
+}
---
+++ R/utilities.R
@@ -0,0 +1,56 @@
+# Fit location-scale family----
+fit_locationScale <- function(lambda, dist) {
+    # Use method of moments
+    muTW <- -1.2065335745820
+    sigmaTW <- sqrt(1.607781034581)
+
+    muS <- mean(log(dist))
+    sigmaS <- stats::sd(log(dist))
+
+    sigma1 <- sigmaS/sigmaTW
+    mu1 <- muS - sigma1 * muTW
+
+    TW <- (log(lambda) - mu1)/sigma1
+    pvalue <- ref_dist(TW)
+
+    return(pvalue)
+}
+
+# This may change to some other function in the future
+ref_dist <- function(x) RMTstat::ptw(x, beta = 1, lower.tail = FALSE, log.p = FALSE)
+
+# Compute test statistic----
+compute_largest_root <- function(first_mat, second_mat) {
+    first_mat_c <- scale(first_mat, center = TRUE, scale = FALSE)
+    W <- crossprod(scale(second_mat, center = TRUE, scale = FALSE))
+
+    svdRes <- corpcor::fast.svd(first_mat_c)
+    Xp <- svdRes$v %*% diag(1/svdRes$d)
+    C <- crossprod(Xp, W %*% Xp)
+    svdC <- corpcor::fast.svd(C)
+    Xpp <- svdC$u
+    singWeights <- Xp %*% Xpp
+    largestRoot <- max(crossprod(singWeights,  W %*% singWeights))
+
+    return(largestRoot)
+}
+
+# Permutations----
+permutation <- function(X, Y, nperm) {
+    if (nrow(X) < nrow(Y)) {
+        first_mat <- X
+        second_mat <- Y
+    } else {
+        first_mat <- Y
+        second_mat <- X
+    }
+    combined_data <- rbind(first_mat, second_mat)
+    n1 <- nrow(first_mat)
+    n2 <- second_mat
+    replicate(nperm, {
+        indices <- sample(1:(n1 + n2), n1)
+        first_mat_perm <- combined_data[indices,]
+        second_mat_perm <- combined_data[which(!1:(n1 + n2) %in% indices),]
+        compute_largest_root(first_mat_perm, second_mat_perm)
+    })
+}
